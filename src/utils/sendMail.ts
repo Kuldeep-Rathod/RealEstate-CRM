af227012225c1decc2e1d5d88323ad301ce0e9f7
Kuldeep Rathod <kuldiprathod375@gmail.com>
2025-03-11 19:39:37 +0530

    implement realtime mail

--- src/utils/sendMail.ts
+++ src/utils/sendMail.ts
@@ -1,34 +1,68 @@
-import nodemailer from "nodemailer";
-import { smtpPass, smtpUser } from "../config/env.js";
+// import nodemailer from "nodemailer";
+// import { smtpPass, smtpUser } from "../config/env.js";
 
-const transporter = nodemailer.createTransport({
-    host: "sandbox.smtp.mailtrap.io",
-    port: 2525,
-    auth: {
-        user: smtpUser,
-        pass: smtpPass,
-    },
-});
+// const transporter = nodemailer.createTransport({
+//     host: "sandbox.smtp.mailtrap.io",
+//     port: 2525,
+//     auth: {
+//         user: smtpUser,
+//         pass: smtpPass,
+//     },
+// });
 
-// async..await is not allowed in global scope, must use a wrapper
-async function sendMail(
-    from: string,
-    to: string,
-    subject: string,
-    text: string,
-    html: string
-) {
-    // send mail with defined transport object
-    const info = await transporter.sendMail({
-        from, // sender address
-        to, // list of receivers
-        subject, // Subject line
-        text, // plain text body
-        html, // html body
-    });
+// // async..await is not allowed in global scope, must use a wrapper
+// async function sendMail(
+//     to: string,
+//     subject: string,
+//     text: string,
+//     html: string
+// ) {
+//     // send mail with defined transport object
+//     const info = await transporter.sendMail({
+//         from: `"Lead Management System" <${smtpUser}>`,
+//         to, // list of receivers
+//         subject, // Subject line
+//         text, // plain text body
+//         html, // html body
+//     });
 
-    console.log("Message sent: %s", info.messageId);
-    // Message sent: <d786aa62-4e0a-070a-47ed-0b0666549519@ethereal.email>
-}
+//     console.log("Mail sent successfully to: ", to);
+// }
 
-export default sendMail;
+// export default sendMail;
+
+//realtime use
+import axios, { AxiosError } from "axios";
+import { mailTemplate } from "./mailTemplate.js";
+
+const MAILTRAP_API_TOKEN = "d200b6ae6416211330e1d3ebd80d068d";
+const MAILTRAP_SENDER = "hello@demomailtrap.co";
+const MAILTRAP_ENDPOINT = "https://send.api.mailtrap.io/api/send";
+
+export const sendWelcomeEmail = async (toEmail: string, name: string) => {
+  try {
+    const response = await axios.post(
+      MAILTRAP_ENDPOINT,
+      {
+        from: {
+          email: MAILTRAP_SENDER,
+          name: "Lead Management System",
+        },
+        to: [{ email: toEmail }],
+        subject: "Welcome to Our Service!",
+        html: mailTemplate(name, "Lead Management System"), // Importing template
+        category: "welcome-email",
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${MAILTRAP_API_TOKEN}`,
+        },
+      }
+    );
+
+    console.log("Email sent successfully:", response.data);
+  } catch (error: AxiosError | any) {
+    console.error("Error sending email:", error.response?.data || error.message);
+  }
+};
