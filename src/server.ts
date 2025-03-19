5c237762575ccb6f1950b239b53bfc2b90a3d3eb
Kuldeep Rathod <kuldiprathod375@gmail.com>
2025-03-19 17:00:49 +0530

    some changes

--- src/server.ts
+++ src/server.ts
@@ -3,4 +3,6 @@ import app from "./app.js";
 const PORT = process.env.PORT || 5000;
 
 // Start the server
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(Number(PORT), "0.0.0.0", () =>
+    console.log(`🚀 Server running on port ${PORT}`)
+);
